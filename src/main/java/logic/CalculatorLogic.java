28b22a80b2bb8e9fb1efc71f0483187dd6cd8900
tomaszlach86 <tomaszlach86@gmail.com>
2019-05-21 14:27:34 +0200

    removed class fields

--- src/main/java/logic/CalculatorLogic.java
+++ src/main/java/logic/CalculatorLogic.java
@@ -9,44 +9,27 @@ public class CalculatorLogic {
     public CalculatorLogic() {
     }
 
-    public CalculatorLogic(double a, double b) {
-        this.a = a;
-        this.b = b;
+    public static boolean isBigger(double a, double b) {
+        return a > b;
     }
 
-    public double getA() {
-        return a;
-    }
-
-    public void setA(double a) {
-        this.a = a;
-    }
-
-    public double getB() {
-        return b;
-    }
-
-    public void setB(double b) {
-        this.b = b;
-    }
-
-    public static boolean isBigger(double a, double b){
-        return a>b;
-    }
-
-    public static boolean isEqual(double a, double b){
+    public static boolean isEqual(double a, double b) {
         return a == b;
     }
 
-    public static double sum(double a, double b){ return a+b;}
-
-    public static double multiply(double a, double b){return a*b;}
-
-    public static double square(double a, double b){
-        return Math.pow(a+b,2);
+    public static double sum(double a, double b) {
+        return a + b;
     }
 
-    public static double podajLiczbe(int i){
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double square(double a, double b) {
+        return Math.pow(a + b, 2);
+    }
+
+    public static double podajLiczbe(int i) {
         Scanner sc = new Scanner(System.in);
         System.out.format("Podaj liczbe %d :", i);
         double output = sc.nextDouble();
